f1fdb9ecd1770c15ec705621f966daddd226f67a
jasper <jasper.hoenderdos@gmail.com>
2021-11-12 14:29:41 +0100

    post & GET
    you can now put the data into a database and get it out with POSTMAN

--- app/Http/Controllers/ClassRoomController.php
+++ app/Http/Controllers/ClassRoomController.php
@@ -7,18 +7,22 @@ use App\Models\ClassRoom;
 
 class ClassRoomController extends Controller
 {
-    // public function createName(Request $request)
-    // {
-    //     if($request->has('name'))
-    //     {
-    //         $name = new Name();
-    //         $name->name = $request->get('name');
-    //         $name->save();
-    //         return response()->json([
-    //             'message' => 'YO je naam is toegevoegd'
-    //         ]);
-    //     }
-    // }
+    public function createData(Request $request)
+    {
+        if($request->has('title'))
+        {
+            $title = new ClassRoom();
+            $title->title = $request->get('title');
+            $title->subimages = $request->get('subimages');
+            $title->description = $request->get('description');
+            $title->headimage = $request->get('headimage');
+            $title->division_id = $request->get('division_id');
+            $title->save();
+            return response()->json([
+                'message' => 'uh oh, het werkt'
+            ]);
+        }
+    }
     public function allData(Request $request)
     {
         $data = ClassRoom::all();
