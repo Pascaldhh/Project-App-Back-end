9850212a3966fdae0f5fdc9ee97269cc91eef955
jasper <jasper.hoenderdos@gmail.com>
2021-11-12 10:41:41 +0100

    dbtables
    tables and databases and stuff

---
+++ database/migrations/2021_11_12_093458_create_class_room_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClassRoomTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('class_room', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->longText('subimages')->nullable();
+            $table->longText('description')->nullable();
+            $table->string('headimage');
+            $table->unsignedBigInteger('division_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('class_room');
+    }
+}
